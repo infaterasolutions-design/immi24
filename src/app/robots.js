75efe27d2ae556ca79e2cb4aaa5eddccc48ecade
Sandeep Singh <infaterasolutions@gmail.com>
2026-05-08 16:44:59 +0530

    feat: add robots.js to allow social media crawlers for OG image previews

---
+++ src/app/robots.js
@@ -0,0 +1,35 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: 'facebookexternalhit',
+        allow: '/',
+      },
+      {
+        userAgent: 'Twitterbot',
+        allow: '/',
+      },
+      {
+        userAgent: 'LinkedInBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'WhatsApp',
+        allow: '/',
+      },
+      {
+        userAgent: 'Slackbot',
+        allow: '/',
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+      },
+    ],
+    sitemap: 'https://www.unitedstatesimmigrationnews.com/sitemap.xml',
+  }
+}
